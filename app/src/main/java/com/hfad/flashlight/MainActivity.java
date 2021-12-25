01ff014fd7e0e21785f9b0a6e9ec919d262d8f2f
akkovaleva14 <32703286+akkovaleva14@users.noreply.github.com>
2021-12-25 10:22:49 +0300

    Added gif, font, pictures. Main layout completed.

--- app/src/main/java/com/hfad/flashlight/MainActivity.java
+++ app/src/main/java/com/hfad/flashlight/MainActivity.java
@@ -2,13 +2,30 @@ package com.hfad.flashlight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView burnHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Load the ImageView that will host the animation and
+        // set its background to our AnimationDrawable XML resource.
+        burnHere = (ImageView)findViewById(R.id.burnHere);
+        burnHere.setBackgroundResource(R.drawable.flame);
+
+        // Get the background, which has been compiled to an AnimationDrawable object.
+        AnimationDrawable frameAnimation = (AnimationDrawable) burnHere.getBackground();
+
+        // Start the animation (looped playback by default).
+        frameAnimation.start();
+
     }
+
 }
